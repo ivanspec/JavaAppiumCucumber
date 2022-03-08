b0e586338ad6367414b86fb24733a320cca64f1f
ivanspec <ivan.jond@gmail.com>
2022-03-08 11:19:50 +0700

    create paging for object

--- src/test/java/stepdefination/LoginStep.java
+++ src/test/java/stepdefination/LoginStep.java
@@ -1,58 +1,31 @@
 package stepdefination;
 
-import io.appium.java_client.MobileElement;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import utility.Hook;
+import page.LoginPage;
 
 public class LoginStep {
 
-    private WebDriver driver;
-
-    public LoginStep() {
-        this.driver = Hook.getDriver();
-    }
-
-    private By loginMenu = By.xpath("//*[@text='Login']");
-    private By inputEmail = By.xpath("//*[@content-desc='input-email']");
-    private By inputPwd = By.xpath("//*[@content-desc='input-password']");
-    private By btnLogin = By.xpath("//*[@content-desc='button-LOGIN']");
-    private By titleAlert = By.id("android:id/alertTitle");
-    private By msgAlert = By.id("android:id/message");
-    private By btnAlert = By.id("android:id/button1");
+    LoginPage Login = new LoginPage();
 
     @Given("User open the app")
     public void useropenTheApp() throws InterruptedException {
-        driver.findElement(loginMenu).click();
-        MobileElement eleLogin = driver.findElement(By.xpath("//*[@content-desc='button-login-container']/*/android.widget.TextView"));
-        eleLogin.click();
-
+        Login.loginMenu();
     }
 
 
     @When("User fill username and password existing")
     public void userFillUsernameAndPasswordExisting() {
-        driver.findElement(inputEmail).sendKeys("test@linkaja.id");
-        driver.findElement(inputPwd).sendKeys("12341234");
-        driver.findElement(btnLogin).click();
+        Login.setUsernameAndPassword();
+        Login.btnLogin();
     }
 
     @Then("User successful login")
     public void userSuccessfulLogin() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, (30));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(titleAlert));
-        String popupTitle = driver.findElement(titleAlert).getText();
-        String messageSuccess = driver.findElement(msgAlert).getText();
-        Assert.assertEquals(popupTitle,"Success");
-        Assert.assertEquals(messageSuccess,"You are logged in!");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(btnAlert));
-        driver.findElement(btnAlert);
+        Login.AssertAlert();
+        Login.closeAlert();
+
     }
 
 
--- src/test/java/stepdefination/RegisterStep.java
+++ src/test/java/stepdefination/RegisterStep.java
@@ -1,62 +1,29 @@
 package stepdefination;
 
-
-import io.appium.java_client.MobileElement;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import utility.Hook;
+import page.RegisterPage;
 
 public class RegisterStep {
-
-    private WebDriver driver;
-
-    public RegisterStep() {
-        this.driver = Hook.getDriver();
-    }
-
-    private By loginMenu = By.xpath("//*[@text='Login']");
-    private By registerTab = By.xpath("//*[@content-desc='button-sign-up-container']/*/android.widget.TextView");
-    private By inputEmail = By.xpath("//*[@content-desc='input-email']");
-    private By inputPwd = By.xpath("//*[@content-desc='input-password']");
-    private By reInputPwd = By.xpath("//*[@content-desc='input-repeat-password']");
-    private By btnSignUp = By.xpath("//*[@content-desc='button-SIGN UP']");
-    private By titleAlert = By.id("android:id/alertTitle");
-    private By msgAlert = By.id("android:id/message");
-    private By btnAlert = By.id("android:id/button1");
+    RegisterPage Register = new RegisterPage();
 
     @Given("^User open the application$")
-    public void UserOpenTheApplication() throws InterruptedException {
-        String loginMenuText = driver.findElement(loginMenu).getText();
-        Assert.assertEquals(loginMenuText,"Login");
-        driver.findElement(loginMenu).click();
-        MobileElement eleSignUp = driver.findElement(registerTab);
-        eleSignUp.click();
+    public void UserOpenTheApplication() {
+        Register.loginMenu();
+        Register.clickRegister();
     }
 
     @When("^User fill username and password$")
     public void UserFillUsernameAndPassword() {
-        driver.findElement(inputEmail).sendKeys("test@linkaja.id");
-        driver.findElement(inputPwd).sendKeys("12341234");
-        driver.findElement(reInputPwd).sendKeys("12341234");
-        driver.findElement(btnSignUp).click();
+        Register.setUsernameAndPassword();
+        Register.clickBtnSignUp();
     }
 
     @Then("^Sucessfull register$")
-    public void sucessfullRegister() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, (30));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(titleAlert));
-        String popupTitle = driver.findElement(titleAlert).getText();
-        String messageSuccess = driver.findElement(msgAlert).getText();
-        Assert.assertEquals(popupTitle,"Signed Up!");
-        Assert.assertEquals(messageSuccess,"You successfully signed up!");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(btnAlert));
-        driver.findElement(btnAlert);
+    public void sucessfullRegister() {
+        Register.assertAlert();
+        Register.closeBtnAlert();
     }
 
 }
